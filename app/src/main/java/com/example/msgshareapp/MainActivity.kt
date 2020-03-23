8c007694257e0593dbddaf9d4d1a711824ee996a
Ashrrith <ashrrithkarunaakar@gmail.com>
2020-03-23 10:20:37 +0530

    Added 2nd activity and implicit intent

--- app/src/main/java/com/example/msgshareapp/MainActivity.kt
+++ app/src/main/java/com/example/msgshareapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.msgshareapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -16,6 +17,26 @@ class MainActivity : AppCompatActivity() {
             Log.i("MainActivity", "Button was clicked !")
 
             Toast.makeText(this, "Button was clicked", Toast.LENGTH_SHORT).show()
+        }
+
+        btnSendMsgToNextActivity.setOnClickListener{
+            val message:String = etUserMessage.text.toString()
+
+            val intent = Intent(this, SecondActivity::class.java)
+
+            intent.putExtra("user_message", message)
+            startActivity(intent)
+        }
+
+        btnShareToOtherApps.setOnClickListener{
+
+            val message : String = etUserMessage.text.toString()
+             val intent = Intent()
+            intent.action = Intent.ACTION_SEND
+            intent.putExtra(Intent.EXTRA_TEXT, message)
+            intent.type = "text/plain"
+
+            startActivity(Intent.createChooser(intent, "Share to :"))
         }
     }
 }
